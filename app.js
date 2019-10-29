8cbf488c9f8c05d412460d96ac6b7827f56b6389
loewentj <38169321+loewentj@users.noreply.github.com>
2019-10-29 14:04:20 +1100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector("#nav-wrap #nav-bar #toggle-img");
-
-const navLinks = document.querySelector(".nav-side");
-
-hamburger.addEventListener("click", () =>{
-    navLinks.classList.toggle("open");
-});
