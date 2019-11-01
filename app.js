a58cdb67bd02d82e756fb44863c52aae33a334de
loewentj <38169321+loewentj@users.noreply.github.com>
2019-11-01 13:53:04 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ hamburger.addEventListener("click", () =>{
     navSide.classList.toggle("open");
 });
 
+
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.querySelectorAll("#showcase .carosel .mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 2000); 
+}
+
